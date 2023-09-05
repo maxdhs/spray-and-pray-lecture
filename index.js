dc706a933f37a96819d04619ba666014cb52a91e
Maxwell Dubin <maxdhs@gmail.com>
2023-09-05 08:16:29 -0400

    ...

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// how do we build a server
-
-// we are going to use express
-// we need to initialize a node project
-
-// when our user sends a GET /  we want to send {success: true, message: "Welcome to the Emoji Server"}
-
 import express from "express";
 
 const app = express();
 
-// our first middleware to tell express to expect json and convert it for us to an object
 app.use(express.json());
 
-// route handler
-// GET request /
-// first argument is the path and the second argument is the cb function to fire (we get req and res arguments for free)
 app.get("/", (req, res) => {
   res.send({ success: true, message: "Welcome to the Emoji Server" });
 });
@@ -24,11 +13,8 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-  // what info do i need from the user?
-  // i need name how do i get it
-  const name = req.body.name;
+  const { name } = req.body;
 
-  // how can we make sure theres a name and reject if not?
   if (!name) {
     return res.send({
       success: false,
@@ -36,10 +22,8 @@ app.post("/users", (req, res) => {
     });
   }
 
-  // i need to create a new user and add it to our "db"
   const user = { id: users.length + 1, name, count: 0 };
 
-  // add the new user to the db
   users.push(user);
   res.send({ success: true, user });
 });
@@ -48,7 +32,6 @@ app.use((req, res) => {
   res.send({ success: false, error: "No route found." });
 });
 
-// express's built in error handling. need 4 paramaters to activate
 app.use((error, req, res, next) => {
   res.send({ success: false, error: error.message });
 });
@@ -58,9 +41,6 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// why are there ports?
-// multiple running apps each want to communicate on the internet to the world
 
 // fake db
 let users = [
